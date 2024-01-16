30220ae9137cd6afb0c9a688c5eed450dfab5e16
Sebastiaan van Doesselaar <sebastiaan.vandoesselaar@expereo.com>
2024-01-16 15:00:40 +0100

    rename and env

--- restore_devcontainer.sh
+++ restore_devcontainer.sh
@@ -1,6 +1,6 @@
 cd ~/dotfiles
 
 helm plugin install https://github.com/jkroepke/helm-secrets --version v4.5.1
-cp scripts/saml_login ~/.local/bin/
+cp scripts/* ~/.local/bin/
 ./scripts/saml2aws.sh
 ./restore_shell.sh
