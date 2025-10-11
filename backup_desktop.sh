c88c4686c1a9bcc30f6b0db61b6a462c56dd5c7a
Sebastiaan van Doesselaar <sebastiaanvandoesselaar@localhost>
2025-10-11 17:28:14 +0200

    ghostty, automount

--- backup_desktop.sh
+++ backup_desktop.sh
@@ -7,6 +7,7 @@ cp -R ~/.config/waybar home/.config/
 cp -R ~/.config/swaync home/.config/
 cp -R ~/.config/wlogout home/.config/
 cp -R ~/.config/kitty home/.config/
+cp -R ~/.config/ghostty home/.config/
 cp -R ~/.config/kanshi home/.config/
 
 # Files
